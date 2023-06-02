dcd64b173079806cb14e7b2171e118d0bc99dead
lucas <klucas5227@gmail.com>
2023-06-02 19:15:15 +0900

    init

---
+++ SwiftGrammar.playground/Pages/3-1.타입안심과 타입추론.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+/// 타입안심과타입추론
+var name: String = "lucas"
+
+//name = 10 //err
+
+
---
+++ SwiftGrammar.playground/Pages/3-2.타입별칭.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/// 타입별칭
+typealias MInt = Int
+typealias YourInt = Int
+typealias MvDouble = Double
+
+let age: MInt = 100
+var year: YourInt = 2080
+// MyInt 는 Int의 또 다른 이름입니다.
+// YourInt도 Int의 또다른이름입니다.
+// MyInt도, YourInt도 Int이기 때문에 같은 타입으로 취급합니다.
+year = age
+let month: Int = 2
+let percentage: MvDouble = 99.9
+
+
