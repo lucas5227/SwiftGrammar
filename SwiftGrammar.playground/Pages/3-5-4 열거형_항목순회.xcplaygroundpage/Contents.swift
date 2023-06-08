afef766be2bc570377cf81a5dbdd5340b4d51204
lucas <klucas5227@gmail.com>
2023-06-08 19:59:28 +0900

    Recursive Enumeration

--- SwiftGrammar.playground/Pages/3-5-4 열거형_항목순회.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/3-5-4 열거형_항목순회.xcplaygroundpage/Contents.swift
@@ -1,82 +1,39 @@
 """
-///열거형_항목순회
+///열거형_순환열거형
 """
-//단순한 열거형에는 CaseIterable 프로토콜을 채택해주는 것만으로 aliCases프로퍼티를 사용할수 있습니다. 그렇지만 조금 복잡해지는 열거형은 그렇지 않을수도 있습니다. 그대표적인예가플랫폼별로사용조건을추가하는경우입니다.
+//열거형 항목의 연관값이 열거형 자신의 값이고자 할때 사용합니다. 순환열거형을 명시하고 싶다면 indirect키워드를 사용 특정항목에만 한정하고 싶다면 case키워드앞에 indirect를 붙이면되고, 열거형 전체에적용하 고싶다면 enum키워드 앞에 indirect 키워드를 붙이면됩니다.
 
-enum School: CaseIterable {
-    case primary
-    case elementary
-    case middle
-    case high
-    case college
-    case university
-    case graduate
+//특정항목에순환열거형항목명시
+enum ArithmeticExpression {
+    case number(Int)
+    indirect case addiction(ArithmeticExpression, ArithmeticExpression)
+    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
 }
 
-let allCases: [School] = School.allCases
-print(allCases) //[School.primary, School.elementary, School.middle, School.high, School.college, School.university, School.graduate]
-
-
-//available속성을통해특정케이스를플랫폼에따라사용할수있거나 없는경우가생기면Caserterable 프로토콜을 채택하는 것만으로는 aLICases 프로퍼티 를 사용할수 없습니다. 그럴때는 직접 a11Cases 프로퍼티를 구현해 주어야 합니다.이렇게 CaseIterable 프로토콜을 채택하여도 allCases 프로퍼티를바로 사용할수없는경우가 또있는데, 바로 열거형의 케이스가 연관값을 갖는 경우입니다.
-enum School2: String, CaseIterable {
-    case primary = "유치원"
-    case elementary = "초등학교"
-    case middle = "중학교"
-    case high = "고등학교"
-    case college = "대학"
-    case university = "대학교"
-    @available(iOS, obsoleted: 12.0)
-    case graduate = "대학원"
-
-    static var allCases: [School2] {
-        let all: [School2] = [.primary,
-                             .elementary,
-                             .middle,
-                             .high,
-                             .college,
-                             .university]
-    #if os(iOS)
-    return all
-    #else
-    return all + [.graduate]
-    #endif
-    }
-                  
+//열거형 전체에 순환 열거형 명시
+indirect enum ArithmeticExpression2 {
+    case number (Int)
+    case addition(ArithmeticExpression2, ArithmeticExpression2)
+    case multiplication(ArithmeticExpression2, ArithmeticExpression2)
 }
 
-let allCases2: [School2] = School2.allCases
-print(allCases2)
-//[School2.primary, School2.elementary, School2.middle, School2.high, School2.college, School2.university, School2.graduate]
+//열거형에는 정수를 연관값으로 갖는 number라는 항목이 있고 덧셈을 위한 adition이라는 항목, 곱셈을 위한 multiplication 항목이있습니다. ArithmeticExpression열거형을 사용하여 (5+4) x 2 연산을 구현해보는 예제입니다.
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addiction(five, four)
+let final = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
 
-//연관값을 갖는 열거형의 항목 순회
-//직접 a11Cases프로퍼티를 구현해주었는데요. 처음 열거형을 정의하고 a11Cases를 구현한 이후에 케이스를 추가할 일이 생긴다면 꼭 잊지말고 a11Cases를 다시 살펴봐야한다.
-enum PastaTaste: CaseIterable {
-    case cream, tomato
-}
-
-enum PizzaDough: CaseIterable {
-    case cheeseCrust, thin, original
-}
-enum PizzaTopping: CaseIterable {
-    case pepperoni, chese, bacon
-}
-enum MainDish: CaseIterable {
-    case pasta (taste: PastaTaste)
-    case pizza(dough: PizzaDough, topping: PizzaTopping)
-    case chiken(withSauce: Bool)
-    case rice
-    
-    static var allCases: [MainDish] {
-        return PastaTaste.allCases.map(MainDish.pasta)
-        + PizzaDough.allCases.reduce([]) { (result, dough) -> [MainDish] in
-            result + PizzaTopping.allCases.map{ (topping) -> MainDish in
-                MainDish.pizza(dough: dough, topping: topping)
-            }
-        }
-        + [true, false].map(MainDish.chiken)
-        + [MainDish.rice]
+// evaluate는 ArithmeticExpression열거형의 계산을 도와주는 순환합수 입니다.
+func evaluate(_expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case .number(let value):
+        return value
+    case .addiction(let left, let right):
+        return evaluate(left) + evaluate(right)
+    case .multiplication(let left, let right):
+        return evaluate(left) * evaluate(right)
     }
 }
 
-print(MainDish.allCases.count) // 14
-print (MainDish.allCases) //모든 경우의 연관값을 갖는케이스 컬렉션
+let result: Int = evaluate(final)
+print("(5+4) * 2 = \(result)") //(5+4) * 2 = 18
