fd999f8af7bda8369dddc0e8749c9669a4652811
lucas <klucas5227@gmail.com>
2023-06-11 19:38:58 +0900

    prefix operator

---
+++ SwiftGrammar.playground/Pages/4-3-1 연산자-사용자 정의_전위 연산자.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+"""
+///사용자 정의 연산자_전위 연산자
+"""
+//전위연산자 구현과 사용
+prefix operator **
+prefix func **(value: Int) -> Int{
+        return value * value
+}
+let minusFive: Int = -5
+let sartMinusFive: Int = **minusFive
+print(sartMinusFive)
+
+//전위연산자함수중복정의와사용
+
+
+prefix func ! (value: String) -> Bool {
+    return value.isEmpty
+}
+
+var stringValue: String = "lucas"
+var isEmptyString: Bool = !stringValue
+print(isEmptyString)            //false
+
+stringValue = ""
+isEmptyString = !stringValue
+print(isEmptyString)            //true
+
+//사용자정의 전위 연산자 함수 중복정의와 사용
+//prefix operator ** 위에서 선언
+prefix func ** (value: String) -> String {
+    return value + " " + value
+}
+
+let resultString: String = **"lucas"
+
+print(resultString) //
