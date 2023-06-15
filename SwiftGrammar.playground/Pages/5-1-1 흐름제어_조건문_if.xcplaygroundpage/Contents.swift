723a94d0c495ac387479d66e5afc023265395ccb
lucas5227 <klucas5227@gmail.com>
2023-06-15 18:01:32 +0900

    switch

--- SwiftGrammar.playground/Pages/5-1-1 흐름제어_조건문_if.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/5-1-1 흐름제어_조건문_if.xcplaygroundpage/Contents.swift
@@ -1,53 +1,54 @@
 """
-///흐름제어_조건문_if
+///흐름제어_조건문_switch
 """
-///if 구문 기본구현
-let first: Int = 5
-let second: Int = 7
+//switch 구문도 소괄호 (())를 생략할수 있습니다. 단, break 키워드 사용은 선택사항입니다. 즉, case 내부의 코드를 모 두 실 행 하 면 b r e a k 없 이 도 s w i t c h 구 문 이 종 료 된 다 는 뜻입니다. 이방식은예상치못한실수를줄이는데도큰도움이됩니다. 따라서 break를 쓰지 않고 Case를 연속실행하던 트릭을 더이상사용하지 못합니다. 스위프트에서swi tch 구문의 case를 연속실행하려면 fallthrough 키워드를 사용합니다.
 
-if first > second {
-    print("first > second")
-}else if first < second {
-    print("first < second")
-}else{
-    print("first == second")
+/// switch구문기본구현
+let integerValue: Int = 5
+
+switch integerValue {
+case 0:
+    print("Value == zero")
+case 1...10:
+    print("Value == 1~10")
+    fallthrough
+case Int.min..<0, 101..<Int.max:
+    print("Value < 0 or Value > 100")
+    break
+default:
+    print("10 < Value <= 100")
 }
- 
-//print("first < second")
 
-///if구문의다양한구현(소괄호가없는코드)
-let first2 = 5
-let second2 = 5
-var biggerValue: Int = 5
+// result
+// Value == 1~10
+// Value < 0 or Value > 100
 
-if (first2 > second2) { //조건 수식을 소괄호로 묶어주는 것은 선택사항임
-    biggerValue = first2
-} else if (first2 == second2) {
-    biggerValue = first2
-} else if (first2 < second2) {
-    biggerValue = second2
-} else if (first2 == 5) {
-    //조건을충족하더라도 이미 first sesecond라는 조건을 충족해 위에서 실행됨
-    biggerValue = 100
+///부동소수타입의 범위연산을 통한 switch case구성
+let doubleValue: Double = 3.0
+
+switch doubleValue {
+case 0:
+    print("Value == zero")
+case 1.5...10.5:
+    print("Value == 1.5~10.5")
+default:
+    print("Value == \(doubleValue)")
 }
-//마지막 else는 생략가능
-print(biggerValue) //5
 
-///if구문의다양한구현(소괄호가 있는코드)
-let first3 = 5
-let second3 = 5
-var biggerValue2: Int = 5
+//Value == 1.5~10.5
 
-if first3 > second3 { //조건 수식을 소괄호로 묶어주는 것은 선택사항임
-    biggerValue2 = first3
-} else if first3 == second3 {
-    biggerValue2 = first3
-} else if first3 < second3 {
-    biggerValue2 = second3
-} else if first3 == 5 {
-    //조건을충족하더라도 이미 first sesecond라는 조건을 충족해 위에서 실행됨
-    biggerValue2 = 100
+///문자열 switch case구성
+let stringvalue: String = "LiamNeeson"
+
+switch stringvalue {
+case "lucas":
+    print("He is lucas")
+case "oracle":
+    print("She is oracle")
+case "neo","trinity","morpheus":
+    print("He or She is \(stringvalue)")
+default:
+    print("\(stringvalue)said' I don't knoww who you are")
 }
-//마지막 else는 생략가능
-print(biggerValue2) //5
 
+//LiamNeesonsaid' I don't knoww who you are
