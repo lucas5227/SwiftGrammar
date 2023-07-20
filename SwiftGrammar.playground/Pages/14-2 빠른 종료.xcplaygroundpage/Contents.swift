81b421f96bbea9ff63ad53a48a235755897cfa3f
lucas5227 <klucas5227@gmail.com>
2023-07-20 16:29:42 +0900

    Trailing Closures

--- SwiftGrammar.playground/Pages/14-2 빠른 종료.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/14-2 빠른 종료.xcplaygroundpage/Contents.swift
@@ -1,75 +1,77 @@
 """
-///맵 Map
+///빠른 종료 Early Exit
 """
 /*
- 맵 MaP은 자신을 호출할 때 매개변수로 전달된 함수를 실행하여 그 결과를 다시 반환해주는 함수이다. 스위프트에서 맵은 배열, 딕셔너리, 세트, 옵셔널등에서 사용할 수 있다. 조금 더 정확히 말하면 스위프트의 Sequence, Collection 프로토콜을 따르는 타입과 옵셔널은 모두 맵을 사용할 수 있다.
- 맵을 사용하면 컨테이너가 담고 있던 각각의 값을 매개변수를 통해 받은 함수에 적용한후 다시 컨테이너에 포장하여 반환한다. 기존 컨테이너의 값은 변경되지않고 새로운 컨테이너가 생성되어반환된다. 그래서 맵은 기존 데이터를 변형하는데 많이 사용한다.
+ 핵심 키워드는 guard
+ guard 구문은 if구문과 유사하게 Bool 타입의 값으로 동작하는 기능이다. guard 뒤에 따라붙는 코드의 실행결과가 true 일때 코드가 계속 실행된다. 1f 구문과는 다르게 guard 구문은 항상 else구문이 뒤에 따라와야 한다. 만약 guard뒤에 따라오는 Boal 값이 false라면 else의 블록 내부코드를 실행하게되는데, 이때 else구문의 블록 내부에는 꼭 자신보다 상위의 코드 블록을 종료하는 코드가 들어가게 된다. 그래서 특정 조건에 부합하지 않다는 관단이 되면 재빠르게 코드 블록의 실행을 종료
+할수 있다. 이렇게 현재의 코드 블록을 종료할때는 return , break , continue , throw등의 제어문 전환 명령을 사용한다.또는 fatalError()와 같은 비반한함수나 메서드를 호출할수도 있다.
  */
-/// for -in 구문과 맵 메서드의 사용 비교
-let numbers: [Int] = [ 0, 1, 2, 3, 4]
+"""
+guard Bool 다입 값 else {
+    예외사항 실행문
+    제어문 전환 명령어
+}
+"""
+/*
+ guard 구문을 사용하면 코드를 훨씬 간결하고 읽기좋게 구성할수 있다 if구문을 사용하면 예외사항을 else 블록으로 처리해야 하지만 예외사항만을 처리하고 싶다면 guard 구문을 사용하는것이 훨씬 간편하다.
+ */
 
-var doubleNumbers: [Int] = [Int]()
-var strings: [String] = [String]()
-
-// for 구문 사용
-for number in numbers {
-    doubleNumbers.append(number * 2)
-    strings.append("\(number)")
+///if 구문과 guard구문의 비교
+///if
+for i in 0...3 {
+    if i == 2 {
+        print(i)
+    } else {
+        continue
+    }
+}
+///guard
+for i in 0...3 {
+    guard i == 2 else {
+        continue
+    }
+    print(i)
 }
 
-print(doubleNumbers)    //[0, 2, 4, 6, 8]
-print(strings)          //["0", "1", "2", "3", "4"]
-
-// map 메서드 사용
-doubleNumbers = numbers.map({ (number: Int) -> Int in
-    return number * 2
-})
-strings = numbers.map({ (number: Int) -> String in
-    return "\(number)"
-})
-
-print(doubleNumbers)                //[0, 2, 4, 6, 8]
-print(strings)                      //["0", "1", "2", "3", "4"]
-
-///클로저 표현의 간략화
-let numbers2: [Int] = [0, 1, 2, 3, 4]
-
-// 기본 클로저 표현식 사용
-var doubleNumbers2 = numbers2.map({ (number: Int) -> Int in
-    return number * 2
-})
-
-//  매개변수 및 반환 타입 생략
-doubleNumbers2 = numbers2.map({ return $0 * 2} )
-print(doubleNumbers2)           //[0, 2, 4, 6, 8]
-
-// 반확 키워드 생력
-doubleNumbers2 = numbers.map({ $0 * 2} )
-print(doubleNumbers2)           //[0, 2, 4, 6, 8]
-
-// 후행 클로저 사용
-doubleNumbers2 = numbers2.map { $0 * 2 }
-print(doubleNumbers2)           //[0, 2, 4, 6, 8]
-
-///다양한 컨테이너 타입에서의 맵의 활용
-let alphabetDictionart: [String: String] = ["a":"A", "b":"B"]
-
-var keys: [String] = alphabetDictionart.map { (tuple: (String, String)) ->
-    String in
-    return tuple.0
+///guard 구문의 옵셔널 바인딩 활용
+func great(_ person: [String: String]) {
+    guard let name: String = person["name"] else {
+        return
+    }
+    
+    print("Hello \(name)!")
+    
+    guard let location: String = person["location"] else {
+        print("I hope the weather is nice near you")
+        return
+    }
+    
+    print("I hope the weather is nice in \(location)")
 }
 
-keys = alphabetDictionart.map{ $0.0 }
+var personInfo: [String: String] = [String: String]()
+personInfo["name"] = "Lucas"
 
-let values: [String] = alphabetDictionart.map { $0.1 }
-print(keys)             //["b", "a"]
-print(values)           //["B", "A"]
+great(personInfo)
+//Hello Lucas!
+//I hope the weather is nice near you
 
-var numberSet: Set<Int> = [1, 2, 3, 4, 5]
-let resultSet = numberSet.map{ $0 * 2 }
-print(resultSet)        //[2, 4, 8, 10, 6]
+personInfo["location"] = "Korea"
 
-let range: CountableClosedRange = (0...3)
-let resultRange: [Int] = range.map{ $0 * 2 }
-print(resultRange)          //[0, 2, 4, 6]
+great(personInfo)
+//Hello Lucas!
+//I hope the weather is nice in Korea
+
+/// guard구문이 사용될 수 없는 경우
+/*
+ guard구문의 한계는 자신을 감싸는 코드블록, 즉return, break, continue, throw등의 제어문 전환명령어를 쓸 수 없는 상황이라면 사용이 불가능하다. 함수나 메서드, 반복문등 특정 블록 내부에 위치하지 않는다면 사용이 제한된다.
+ 
+let first: Int = 3
+let second: Int = 5
+
+guard first > second else {
+    // 여기에 들어올 제어문 전환 명령은 딱히 없다. err!
+}
+ */
+
 
