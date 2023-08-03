a7129145ad28951f9faecfa665b766bc6f112244
lucas <klucas5227@gmail.com>
2023-08-03 19:50:10 +0900

    Multiple Subscripts

--- SwiftGrammar.playground/Pages/17 서브 스크립트.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/17 서브 스크립트.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -74,3 +74,96 @@ highScool.addStudents(names: "Lucas", "Neo", "Trinity")
 let aStudent: Student? = highScool[1]
 print("\(aStudent?.number) \(aStudent?.name)")          //Optional(1) Optional("Neo")
 
+"""
+17-3 복수 서브스크립트
+매개변수 타입을 하용하여 서브스크립트를 구현하면 하나의 타입이 여러 개의 서브스크립트를 가질수 있다.
+"""
+/// 복수의 서브스크립트 구현
+struct Student2 {
+    var name: String
+    var number: Int
+}
+
+class School2 {
+    
+    var number : Int = 0
+    var students: [Student2] = [Student2]()
+    
+    func addStudent(name: String){
+        let student: Student2 = Student2(name: name, number: self.number)
+        self.students.append(student)
+        self.number += 1
+    }
+    
+    func addStudents(names: String...) {
+        for name in names {
+            self.addStudent(name: name)
+        }
+    }
+    subscript(index: Int) -> Student2? {        // 첫 번째 서브스크립트
+        get {
+            if index < self.number {
+                return self.students[index]
+            }
+            return nil
+        }
+        
+        set {
+            guard var newStudent: Student2 = newValue else {
+                return
+            }
+            
+            var number: Int = index
+            
+            if index > self.number {
+                number = self.number
+                self.number += 1
+            }
+            
+            newStudent.number = number
+            self.students[number] = newStudent
+        }
+    }
+    
+    subscript(name: String) -> Int? {           // 두 번째 서브스크립트
+        get {
+            return self.students.filter { $0.name == name }.first?.number
+        }
+        
+        set {
+            guard var number: Int = newValue else {
+                return
+            }
+            
+            if number > self.number {
+                number = self.number
+                self.number += 1
+            }
+            
+            let newStudent: Student2 = Student2(name: name, number: number)
+            self.students[number] = newStudent
+        }
+    }
+    subscript(name: String, number: Int) -> Student2? {         // 새 번째 서브스크립트
+        return self.students.filter{ $0.name == name && $0.number == number }.first
+    }
+}
+
+let highScool2: School2 = School2()
+highScool2.addStudents(names: "Lucas", "Neo", "Trinity")
+
+let aStudent2: Student2? = highScool2[1]
+print("\(aStudent2?.number) \(aStudent2?.name)")    //Optional(1) Optional("Neo")
+
+print(highScool2["Oracle"])         //nil
+print(highScool2["Lucas"])          //Optional(0)
+
+highScool2[0] = Student2(name: "SungMin", number: 0)
+highScool2["KL"] = 1
+
+print(highScool2["Lucas"])              //nil
+print(highScool2["KL"])                 //Optional(1)
+print(highScool2["Trinity", 2])         //Optional(Student2(name: "Trinity", number: 2))
+print(highScool2["Oracle", 3])         //nil
+
+    
