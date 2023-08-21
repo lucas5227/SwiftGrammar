2b53094b20fbdf7b7df79b6d38ac2f0ef0a94194
lucas5227 <klucas5227@gmail.com>
2023-08-21 15:49:32 +0900

    Override Property Observer

--- SwiftGrammar.playground/Pages/18-2 상속-재정의.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/18-2 상속-재정의.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -110,7 +110,72 @@ print(neo2.koreanAge)                   //15
 
 /**
  * 8-2-3 프로퍼티 감시자 재정의
+ * 프로퍼티 감시자도 프로퍼티의 접근자와 설정자처럼 재정의 할수 있다. 또 조상 클래스에 정의한 프로퍼티가 연산 프로퍼티인지 저장프로퍼티인지는 상관없다. 다만 상수지장 프로퍼티나 읽기 전용 연산프로퍼티는 프로퍼디 감시자를 재정의 할수 없다. 왜냐하면 상수 저장 프로퍼티나 읽기 전용 연산 프로퍼티는 값을 설정할 수 없으므로 willSet 이나 didSet 메서드를 사용한 프로퍼티 감시자를 원천직으로 사용할 수 없기 때문이다. 또, 프로퍼티 감시자를 재정의하더라도 조상클래스에 정의한 프로퍼티 감시자도 동작한다는점, 잊지말아야 한다. 프로퍼티의 접근자와 프로퍼티 감시자는 동시에 재정의할 수 없다. 만약 둘 다 동작하길 원한다면 재정의하는 점근자에 프로퍼티 감시자의 역할을 구현해야 한다.
  */
+class Person3 {
+    var name: String = ""
+    var age: Int = 0 {
+        didSet {
+            print("Person age: \(self.age)")
+        }
+    }
+    var koreanAge: Int {
+        return self.age + 1
+    }
+    
+    var fullName: String {
+        get {
+            return self.name
+        }
+        
+        set {
+            self.name = newValue
+        }
+    }
+}
+class Studunt3: Person3 {
+    var grade: String = "F"
+    override var age: Int {
+        didSet {
+            print("Student age : \(self.age)")
+        }
+    }
+    
+    override var koreanAge: Int {
+        get {
+            return super.koreanAge
+        }
+        
+        set {
+            self.age = newValue - 1
+        }
+        
+        //didSet { }          //err
+    }
+    
+    override var fullName: String {
+        didSet {
+            print("Full Name : \(self.fullName)")
+        }
+    }
+}
+
+let lucas3: Person3 = Person3()
+lucas3.name = "lucas"
+lucas3.age = 99
+lucas3.fullName = "Lucas Kim"
+print(lucas3.koreanAge)
+
+let neo3: Studunt3 = Studunt3()
+neo3.name = "neo"
+neo3.age = 1
+//Person age: 1
+//Student age : 1
+neo3.koreanAge = 3
+//Person age: 2
+//Student age : 2
+neo3.fullName = "NEO"           //Full Name : NEO
+print(neo3.koreanAge)           //3
 
 /**
  * 8-2-4 서브스크립트재정의
