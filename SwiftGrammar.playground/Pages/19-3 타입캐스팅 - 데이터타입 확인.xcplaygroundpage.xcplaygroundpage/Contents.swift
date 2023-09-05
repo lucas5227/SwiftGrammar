57a5294fc3c2e328848d2859a45028aa33e0eea3
lucas <lucas@n-os.com>
2023-09-05 16:42:27 +0900

    missing

--- SwiftGrammar.playground/Pages/19-3 타입캐스팅 - 데이터타입 확인.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/19-3 타입캐스팅 - 데이터타입 확인.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -5,3 +5,50 @@
 /*
  타입 확인 연산자인 is를 사용하여 인스턴스가 어떤 클래스(혹은 어떤클래스의 자식 클래스)의 인스턴스인지 타입을 확인해볼수 있다. 타입 확인 연산자는 인스턴스가 해당 클래스의 인스턴스거나 그 자식 클레스의 인스턴스라면 true를 반환하 고, 그렇지 않다면 false를 반환한다. is연산자는 클래스의 인스턴스 뿐만 아니라 모든 데이터 타입에 사용 할 수 있다.
  */
+///데이터 타입확인
+class Coffee {
+    let name: String
+    let shot: Int
+    
+    var description: String {
+        return "\(shot) shot(s) \(name)"
+    }
+    
+    init(shot: Int) {
+        self.shot = shot
+        self.name = "coffee"
+    }
+}
+
+class Latte: Coffee {
+    var flavor: String
+    
+    override var description: String {
+        return "\(shot) shot(s) \(flavor) latte"
+    }
+    
+    init(flavor: String, shot: Int) {
+        self.flavor = flavor
+        super.init(shot: shot)
+    }
+}
+
+class Americano: Coffee {
+    let iced: Bool
+    
+    override var description: String {
+        return "\(shot) shot(s) \(iced ? "iced" : "hot") americano"
+    }
+    
+    init(shot: Int, iced: Bool) {
+        self.iced = iced
+        super.init(shot: shot)
+    }
+}
+
+///
+let coffee: Coffee = Coffee(shot: 1)
+print(coffee.description)           //1 shot(s) coffee
+
+let myCoffee: Americano = Americano(shot: 2, iced: false)
+print(myCoffee.description)           //2 shot(s) hot americano
