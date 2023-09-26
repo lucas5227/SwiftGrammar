27d8ae5afab6ba1ba4f98fdac7b7abd3f5af3310
lucas5227 <klucas5227@gmail.com>
2023-09-26 15:42:56 +0900

    missing

--- SwiftGrammar.playground/Pages/20-3 프로토콜-프로토콜 상속 & 클래스 프로토콜.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/20-3 프로토콜-프로토콜 상속 & 클래스 프로토콜.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -1,3 +1,34 @@
 """
 ///프로토콜 - 프로토콜 상속 & 클래스 전용 프로토콜
 """
+
+///프로토콜의 상속
+protocol Readable {
+    func read()
+}
+
+protocol Writeable {
+    func write()
+}
+
+protocol ReadSpeakable: Readable {
+    func speak()
+}
+
+protocol ReadWriteSpeakable: Readable, Writeable {
+    func speak()
+}
+
+class SomeClass: ReadWriteSpeakable {
+    func read() {
+        print("Read")
+    }
+    
+    func write() {
+        print("Write")
+    }
+    
+    func speak() {
+        print("Speak")
+    }
+}
