d1539bd34aa1d32a0bd6dd1623777afe34173a88
lucas <klucas5227@gmail.com>
2023-10-01 10:46:56 +0900

    missing

--- SwiftGrammar.playground/Pages/20-5 프로토콜-프로토콜의 선택적 요구.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/20-5 프로토콜-프로토콜의 선택적 요구.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -1,4 +1,19 @@
 """
 ///프로토콜 - 프로토콜의 선택적 요구 @objc
+///objc속성을 사용하려면 애플의 Foundation프레임워크 모들을 임포트해야한ㄷ.
 """
-///프로토콜 조합 및 프로토콜, 클래스 조합
+///프로토콜의 선택적 요구
+import Foundation
+
+@objc protocol Noveable {
+    func walk()
+    @objc optional func fly()
+}
+
+//걷기만 할 수 있는 호랑이
+class Tiger: NSObject, Noveable {
+    func walk() {
+        print("Tieger walks")
+    }
+}
+
