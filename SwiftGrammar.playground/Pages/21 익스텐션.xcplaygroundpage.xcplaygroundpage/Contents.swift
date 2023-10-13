524c04fb1d73d105a614bcddd86919d53635d8ef
lucas <klucas5227@gmail.com>
2023-10-13 13:19:52 +0900

    Extansion

--- SwiftGrammar.playground/Pages/21 익스텐션.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/21 익스텐션.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -287,3 +287,42 @@ extension String {
 print("abc"["def"])                 //abcdef
 print("abc"[3])                     //abcabcabc
 
+///21.3.5 중첩 데이터 타입
+///익스텐션을 통한 중첩 데이터 타입 투가
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+print(1.kind)                   //positive
+print(0.kind)                   //zero
+print((-1).kind)                //negative
+
+func printIntegerKinds(numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case . negative:
+            print("- ", terminator:  "")
+        case .zero:
+            print("0 ", terminator:  "")
+        case .positive:
+            print("+ ", terminator:  "")
+        }
+    }
+    print("" )
+}
+printIntegerKinds(numbers: [3, 19, -27, 0, -6, 0, 7])
+//+ + - 0 - 0 +
+
