74102eb578ca4e3663c118906ffdfdea28375621
lucas5227 <klucas5227@gmail.com>
2023-10-17 17:30:39 +0900

    Generic

--- SwiftGrammar.playground/Pages/22-1 제네릭 함수.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/22-1 제네릭 함수.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -29,4 +29,5 @@ public struct Array<Element> : RandomAccessCollection, MutableCollection {
     //중략...
     public func reduce<Result>(_ initialResult: Result, _ nextPartialResult: (Result, Element) throws -> Result) rethrows -> Result
 }
+ 
 */
