0af0642105f92f35cdafa1272c2460ae005ba06d
lucas5227 <klucas5227@gmail.com>
2023-10-25 09:59:53 +0900

    Generic type

--- SwiftGrammar.playground/Pages/22-2 제네릭 타입.xcplaygroundpage.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/22-2 제네릭 타입.xcplaygroundpage.xcplaygroundpage/Contents.swift
@@ -64,4 +64,22 @@ print(doubleStack.items)            //[1.0, 2.0]
 doubleStack.pop()
 print(doubleStack.items)            //[1.0]
 
-var stringStack: Stack<String>
+var stringStack: Stack<String> = Stack<String>()
+
+stringStack.push("1")
+print(stringStack.items)            //["1"]
+stringStack.push("2")
+print(stringStack.items)            //["1", "2"]
+stringStack.pop()
+print(stringStack.items)            //["1"]
+
+var anyStack: Stack<Any> = Stack<Any>()
+
+anyStack.push(1.0)
+print(anyStack.items)           //[1.0]
+anyStack.push("2")
+print(anyStack.items)           //[1.0, "2"]
+anyStack.push(3)
+print(anyStack.items)           //[1.0, "2", 3]
+anyStack.pop()
+print(anyStack.items)           //[1.0, "2"]
