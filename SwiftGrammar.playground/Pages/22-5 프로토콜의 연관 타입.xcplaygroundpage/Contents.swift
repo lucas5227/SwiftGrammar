ce900510c8c6a4e950b83f2edb43d039da1fb2ce
lucas5227 <klucas5227@gmail.com>
2023-11-08 14:27:28 +0900

    Associate Type

--- SwiftGrammar.playground/Pages/22-5 프로토콜의 연관 타입.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/22-5 프로토콜의 연관 타입.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 /// 22-5 프로토콜의 연관 타입
 프로토콜을 정의할때 연관타입을 함께 정의하면 유용할때가 있다. 연관타입은 프로토콜에서 사용할 수 있는 플레이스 홀더 이름이다. 즉, 제네릭에서는 어떤 타입이 들어울지 모를 때, 타입 매개변수를 통해 종류는 알 수 없지만, 어떤 타입이 여기에 쓰일 것이다 라고 표현 해주었다면 연관 타입은 타입 매개변수의 그 역할을 프로토콜에서 수행할 수 있도록 만들어진 기능이다.
 """
-///Conysinrt 프로토콜의 정의
+///Container 프로토콜의 정의
 /*
  Container프로토콜은 존재하지 않는 타입인 ItemType을 연관 타입으로 정의하여 프로토콜 정의에서 타입 이름으로 활용한다. 이는 제네릭의 타입 매개변수와 유사한기능으로,프로토콜 정의 내부에서 사용할 타입이 "그어떤것이어도상관없지만, 하나의 타입임은 분명하다라는 의미입니다. Container프로토콜을 준수하는 타입이 꼭 구현해야 할 기능을 명시하면
     • 컨테이너의 새로운 아이템을 apend(_:) 메서드를 통해 추가한 수 있어야한다.
@@ -29,6 +29,29 @@ class MyContainer: Container {
         item.append(item)
     }
     
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+}
+
+/// IntStack 구조체의 Container 프로토콜 준수
+struct IntStack: Container {
+    //기존 IntStack 구조체 구현
+    var items = [Int]()
+    mutating func puch(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+    
+    //Container 프로토콜 준수를 위한 구현
+    mutating func append(_ item: Int) {
+        self.puch(item)
+    }
+    var count: Int {
+        return items.count
+    }
     subscript(i: Int) -> Int {
         return items[i]
     }
