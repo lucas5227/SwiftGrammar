4b37542f365588bf8f98c2091c860e9fca52ad80
lucas5227 <klucas5227@gmail.com>
2023-11-09 13:55:43 +0900

    Associate Type

--- SwiftGrammar.playground/Pages/22-5 프로토콜의 연관 타입.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/22-5 프로토콜의 연관 타입.xcplaygroundpage/Contents.swift
@@ -56,3 +56,51 @@ struct IntStack: Container {
         return items[i]
     }
 }
+
+/// IntStack 구조체의 typealias 사용
+struct IntStack: Container {
+    typealias ItemType = Int
+    
+    // 기존 IntStack 구조체 구현
+    var items = [ItemType]()
+    mutating func push(_ item: ItemType) {
+        items.append(item)
+    }
+    mutating func pop() -> ItemType {
+        return items.removeLast()
+    }
+    
+    // Container 프로토콜 준수를 위한 구현
+    mutating func append(_ item: ItemType) {
+        self.push(item)
+    }
+    var count: ItemType {
+        return items.count
+    }
+    subscript(i: ItemType) -> ItemType {
+        return items[i]
+    }
+}
+
+/// Stack 구조체의 Container 프로토콜 준수
+struct Stack<Element>: Container {
+    // 기존 Stack<Element> 구조체 구현
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    
+    // Container 프로토콜 준수를 위한 구현
+    mutating func append(_ item: Element) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
