08991f2cbb066d6a89659d0dc468a8ad435c50f9
lucas5227 <klucas5227@gmail.com>
2023-11-30 17:11:41 +0900

    Nested Type

--- SwiftGrammar.playground/Pages/24-1 중첩 데이터 타입.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/24-1 중첩 데이터 타입.xcplaygroundpage/Contents.swift
@@ -1,3 +1,33 @@
 """
-///
+///24-1 중첩 데이터 타입 Nested Type
 """
+///중첩데이터타입구천
+class Person {
+    enum Job {
+        case jobless, programer, student
+    }
+    
+    var job: Job = .jobless
+}
+
+class Student: Person {
+    enum School {
+        case elementary, middle, high
+    }
+    
+    var school: School
+    
+    init(school: School) {
+        self.school = school
+        super.init()
+        self.job = .student
+    }
+}
+
+let personJob: Person.Job = .jobless
+let studentJob: Student.Job = .student
+
+let student: Student = Student(school: .middle)
+
+print(student.job)                  //student
+print(student.school)               //middle
