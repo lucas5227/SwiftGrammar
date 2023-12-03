20031c4dc73b2e25e1cb10f52765680506a882d5
lucas <klucas5227@gmail.com>
2023-12-03 16:50:27 +0900

    Nested Type

--- SwiftGrammar.playground/Pages/24-1 중첩 데이터 타입.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/24-1 중첩 데이터 타입.xcplaygroundpage/Contents.swift
@@ -33,4 +33,56 @@ print(student.job)                  //student
 print(student.school)               //middle
 
 ///같은 이름의 중첩 데이티 타입 구현
-///
+struct Sports {
+    enum GameType {
+    case football, basketball
+    }
+    
+    var gameType: GameType
+    
+    struct GameInfo {
+        var time: Int
+        var player: Int
+    }
+    
+    var gameInfo: GameInfo {
+        switch self.gameType {
+        case .basketball:
+            return GameInfo(time: 40, player: 5)
+        case .football:
+            return GameInfo(time: 60, player: 11)
+        }
+    }
+}
+
+struct ESports {
+    enum GameType {
+    case online, offline
+    }
+    
+    var gameType: GameType
+    
+    struct GameInfo {
+        var location: String
+        var pakage: String
+    }
+    
+    var gameInfo: GameInfo {
+        switch self.gameType {
+        case .online:
+            return GameInfo(location: "www.liveonline.co.kr", pakage: "LoL")
+        case .offline:
+            return GameInfo(location: "제주", pakage: "SA")
+        }
+    }
+}
+
+var basketball: Sports = Sports(gameType: .basketball)
+print(basketball.gameInfo)          //GameInfo(time: 40, player: 5)
+
+var sudden: ESports = ESports(gameType: .offline)
+print(sudden.gameInfo)              //GameInfo(location: "제주", pakage: "SA")
+
+let someGameType: Sports.GameType = .football
+let anotherGameType: ESports.GameType = .online
+//let errorIfYouWantIt: Sports.GameType = .online           //err
