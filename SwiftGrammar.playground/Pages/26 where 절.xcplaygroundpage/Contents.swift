8521078094f74d575d812de7e44330ed58fd01d5
lucas5227 <klucas5227@gmail.com>
2023-12-18 17:00:49 +0900

    Generic Where

--- SwiftGrammar.playground/Pages/26 where 절.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/26 where 절.xcplaygroundpage/Contents.swift
@@ -167,3 +167,68 @@ Double(8.0).printSelf()
 
 //그 외 SelfPrintable을 준수하는 타입 Person
 Person().printSelf()
+
+///where 절을 활용한 타입 매개변수와 연관 차입의 차입 제약 추가
+//타입 매개변수 T가 BinaryInteger 프로토콜을 준수하는 타입
+func doubled<T>(integerValue: T) -> T where T: BinaryInteger {
+    return integerValue * 2
+}
+
+//위 함수와 같은 표현입니다.
+func doubled<T: BinaryInteger>(integerValue: T) -> T {
+    return integerValue * 2
+}
+
+//타입 매개변수 T와 U가 CustomStringConvertible 프로토콜을 준수하는 타입
+func prints<T, U>(first: T, second: U) where T: CustomStringConvertible, U:
+CustomStringConvertible {
+    print(first)
+    print(second)
+}
+
+//위 함수와 같은 표현
+func prints_2<T: CustomStringConvertible, U:
+                    CustomStringConvertible>(first: T, second: U) {
+    print(first)
+    print(second)
+}
+
+//타입 매개변후 S1과 S2가 Sequence 프로토콜을 준수하며
+//S1과 S2가 준수하는 프로토콜인 Sequence 프로토콜의 연관 타입인 Element가 같은 타입
+func compareTwoSequences<S1, S2>(a: S1, b: S2) where S1: Sequence,
+S1.Element: Equatable, S2: Sequence, S2.Element: Equatable {
+    //...
+}
+
+//프로토콜의 연관 차입에도 타입 제앾을 줄 수 있다.
+protocol Container {
+    associatedtype ItemType where ItemType: BinaryInteger
+    var count: Int { get }
+}
+//위 표현과 같은 표현이다.
+protocol Container where ItemType: BinaryInteger {
+    associatedtype ItemType
+    var count: Int { get }
+    
+    mutating func append(_ item: ItemType)
+    subscript(i: Int) -> ItemType { get }
+}
+
+///where 절을 활용한 제네릭 타입의 연관 타입 제약 추가
+protocol Talkable { }
+protocol CallToAll {
+    func callToAll()
+}
+
+struct Person_2: Talkable { }
+struct Animal { }
+
+extension Array: CallToAll where Element: Talkable {
+    func callToAll() { }
+}
+
+let people: [Person_2] = []
+let cats: [Animal] = []
+
+people.callToAll()
+// cats.callToAll()
