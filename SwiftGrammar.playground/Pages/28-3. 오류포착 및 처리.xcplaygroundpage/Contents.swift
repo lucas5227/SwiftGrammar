aa7f4d9648b139450145961a8d2ca4d01ae095d6
lucas5227 <klucas5227@gmail.com>
2024-03-04 18:41:48 +0900

    rethrows

--- SwiftGrammar.playground/Pages/28-3. 오류포착 및 처리.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/28-3. 오류포착 및 처리.xcplaygroundpage/Contents.swift
@@ -268,3 +268,39 @@ do {
 
 
 ///다시 던지기 함수의 오류 던지기
+//오류를 던지는 함수
+func someThrowingFunction_4() throws {
+    enum SomeError: Error {
+        case justSomeError
+    }
+    
+    throw SomeError.justSomeError
+}
+
+//다시 던지기 함수
+func someFunction_2(callback: () throws -> Void) rethrows {
+    enum AnotherError: Error {
+        case justAnotherError
+    }
+
+
+do {
+    //매개변수로 전달한 오류 던지기 함수이므로
+    //catch 절에서 제어할 수 있다.
+    try cal;back()
+} catch {
+    throw AnotherError.justAnotherError
+}
+
+do {
+    //매개변수로 전달한 오류 던지기 함수가 아니므루
+    //catch 절에서 제어할 수 없다.
+    try someThrowFunction_3()
+} catch {
+    //오류 발생!
+    throw AnotherError.justAnotherError
+}
+
+//catch 절 외부에서 단독으로 오류를 던질 수는 없다. err!
+throw Another.justAnotherError
+}
