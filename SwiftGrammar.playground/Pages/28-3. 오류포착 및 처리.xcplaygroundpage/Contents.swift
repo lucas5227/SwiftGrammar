3a93d427e86157ec18b8fb741368ec785fac9e30
lucas5227 <klucas5227@gmail.com>
2024-03-12 14:01:57 +0900

    defer

--- SwiftGrammar.playground/Pages/28-3. 오류포착 및 처리.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/28-3. 오류포착 및 처리.xcplaygroundpage/Contents.swift
@@ -362,3 +362,25 @@ for i in 0...2 {
     // A 1 A 2 It's even
     // A
 }
+
+///28-10 파일쓰기 예제에서 defer 구문 활용
+/*
+func writeData() {
+    let file = openFile()
+    
+    //함수 코드 블록을 빠져나가기 직전 무조건 실행되어 파일을 잊지 않고 닫아 준다.
+    defer {
+        closeFile(file)
+    }
+    
+    if ... {
+        return
+    }
+    
+    if ... {
+        return
+    }
+    
+    //처리 끝
+}
+*/
