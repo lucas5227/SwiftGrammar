ad99fb6e21dcad02b51c4222e923d31c43ba1a46
lucas5227 <klucas5227@gmail.com>
2024-03-13 18:39:35 +0900

    defer

--- SwiftGrammar.playground/Pages/28-3. 오류포착 및 처리.xcplaygroundpage/Contents.swift
+++ SwiftGrammar.playground/Pages/28-3. 오류포착 및 처리.xcplaygroundpage/Contents.swift
@@ -384,3 +384,25 @@ func writeData() {
     //처리 끝
 }
 */
+
+///파일쓰기 에제에서 defer 구문이 없다면
+/*
+func writeData() {
+    let file = openFile()
+    
+    if ... {
+        closeFile(file)
+        return
+    }
+    
+    if ... {
+        closeFile(file)
+        return
+    }
+    
+    
+    closeFile(file)
+    // 처리 끝
+    
+}
+*/
